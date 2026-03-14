71f4cd0070455a292f2987a244446942f65babad
samuelyingcx <samuelyingcx@gmail.com>
2026-03-15 01:53:33 +0800

    chore: set pm2 port to 3001

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
       max_memory_restart: '512M',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000,
+        PORT: 3001,
         DATABASE_URL: 'file:./prisma/prod.db',
       },
     },
